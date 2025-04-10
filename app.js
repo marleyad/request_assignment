3a6a1803b11c5bd537d502cb0d10666a4df0b1fe
Adam Marley <141704661+marleyad@users.noreply.github.com>
2025-04-10 18:31:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const $gifArea = $("#gif-area");
 const $searchInput = $("#search");
 
 /* Use Ajax result to add a gif */
-
+ 
 function addGif(res) {
   let numResults = res.data.length;
   if (numResults) {
